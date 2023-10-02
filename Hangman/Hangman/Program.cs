3766e797cc2b2493586491795813aa9a939f6ab3
Regboy7 <145344365+Regboy7@users.noreply.github.com>
2023-10-02 11:40:35 +0100

    Add files via upload

---
+++ Hangman/Hangman/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the secret word");
+            string SecretWord = Console.ReadLine();
+            char[] charlist = SecretWord.ToCharArray();
+            foreach (char s in charlist)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
