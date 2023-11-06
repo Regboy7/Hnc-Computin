1916044a6553b52de9060ccb057432d92f2dbe43
ISAAC HAIN <ISAACHAIN@exe-coll.ac.uk>
2023-11-06 11:25:51 +0000

    a

--- Minesweeper code/Minesweeper code/MinesweeperGame.cs
+++ Minesweeper code/Minesweeper code/MinesweeperGame.cs
@@ -14,6 +14,13 @@ namespace Minesweeper_code
 {
     public partial class MinesweeperGame : Form
     {
+        private const int Rows = 10;
+        private const int Columns = 10;
+        private const int Mines = 15;
+        private Button[,] buttons1 = new Button[Rows, Columns];
+        private Button[,] buttons2 = new Button[Rows, Columns];
+        private bool[,] Minehit1 = new bool[Rows, Columns];
+        private bool[,] Minehit2 = new bool[Rows, Columns];
         public string p1ID;
         public string p2ID;
         public string P1User;
@@ -23,24 +30,72 @@ namespace Minesweeper_code
         {
 
             InitializeComponent();
+
             this.p1ID = P1UID.ToString();
             this.p2ID = P2UID.ToString();
             this.P1User = DatabaseCon.Datalist("Username", "Scoreboard", p1ID);
             this.P2User = DatabaseCon.Datalist("Username", "Scoreboard", p2ID);
-            tableLayoutPanel1.ColumnStyles.Clear();
-            tableLayoutPanel1.RowStyles.Clear();
-            for (int i = 0; i < 3; i++)
+        }
+        private void InitializeGame(TableLayoutPanel tableLayoutPanel, Button[,] buttons, bool[,] Minehit)
+        {
+            InitializeGrid(tableLayoutPanel, buttons);
+            PutMines(Minehit);
+            PlayGame(P1User, P2User);
+        }
+        private void InitializeGrid(TableLayoutPanel tableLayoutPanel, Button[,] buttons)
+        {
+            tableLayoutPanel.RowCount = Rows;
+            tableLayoutPanel.ColumnCount = Columns;
+            tableLayoutPanel.ColumnStyles.Clear();
+            tableLayoutPanel.RowStyles.Clear();
+
+            for (int row = 0; row < Rows; row++)
             {
-                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33F));
-                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.33F))
+                tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / Rows));
+                for (int col = 0; col < Columns; col++)
+                {
+                    buttons[row, col] = new Button
+                    {
+                        Name = $"button_{row}_{col}",
+                        Dock = DockStyle.Fill,
+                        Tag = new CellData(row, col),
+                    };
+                    buttons[row, col].Click += Button_Click;
+                    tableLayoutPanel.Controls.Add(buttons[row, col], col, row);
+                }
             }
-;
+        }
+
+        private void PutMines(bool[,] Minehit)
+        {
+            Random random = new Random();
+            for (int i = 0; i > Mines; i++)
+            {
+                int row, col;
+                do
+                {
+                    row = random.Next(0, Rows);
+                    col = random.Next(0, Columns);
+                }
+                while (Minehit[row, col]);
+                {
+                    Minehit[row, col] = true;
+                }
+            }
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            CellData cellData = (CellData)button.Tag;
+            int row = cellData.Row;
+            int col = cellData.Column;
         }
 
         public void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(P1User, P2User);
+            // NOT IN USE
         }
+
 
         private void MinesweeperGame_Load(object sender, EventArgs e)
         {
@@ -48,6 +103,33 @@ namespace Minesweeper_code
             label2.Text = P2User;
             label3.Text = P1User + "has cleared 0 tiles";
             label4.Text = P2User + "has cleared 0 tiles";
+        }
+        public void player1go(bool foundmin3)
+        {
+
+        }
+        public void PlayGame(string P1, string P2)
+        {
+            int i = 2;
+            bool foundmine = false;
+            while (foundmine = false && i < 100)
+            {
+                if (i % 2 == 0)
+                {
+
+                }
+            }
+        }
+    }
+    class CellData
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public CellData(int row, int column)
+        {
+            Row = row;
+            Column = column;
         }
     }
 }
