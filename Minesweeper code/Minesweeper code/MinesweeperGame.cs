3dab35dd0e90f45ea23556cb59607d09c9c57f61
ISAAC HAIN <ISAACHAIN@exe-coll.ac.uk>
2023-11-13 11:46:54 +0000

    a

--- Minesweeper code/Minesweeper code/MinesweeperGame.cs
+++ Minesweeper code/Minesweeper code/MinesweeperGame.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -14,23 +15,51 @@ namespace Minesweeper_code
 {
     public partial class MinesweeperGame : Form
     {
-        private const int Rows = 10;
-        private const int Columns = 10;
-        private const int Mines = 15;
-        private Button[,] buttons1 = new Button[Rows, Columns];
-        private Button[,] buttons2 = new Button[Rows, Columns];
-        private bool[,] Minehit1 = new bool[Rows, Columns];
-        private bool[,] Minehit2 = new bool[Rows, Columns];
+        private const int Rows = 5;
+        private const int Columns = 5;
+        private const int Mines = 5;
+        private const int QuestionsInterval = 3;
+
+        private Button[,] player1Buttons = new Button[Rows, Columns];
+        private Button[,] player2Buttons = new Button[Rows, Columns];
+        private bool[,] player1IsMine = new bool[Rows, Columns];
+        private bool[,] player2IsMine = new bool[Rows, Columns];
+        private int player1Score = 0;
+        private int player2Score = 0;
+        private int turnCounter = 0;
+        private List<string> questions = new List<string>
+                                    { "What is the official name of the UK's Army, Navy, and Air Force?",
+                                    "What is the highest rank in the British Army?",
+                                    "What is the largest and most powerful warship in the Royal Navy's fleet?",
+                                    "What is the British Army's elite special forces unit known as?",
+                                    "Which conflict is often referred to as the Falklands War?",
+                                    " in which the UK fought against Argentina?",
+                                    "What is the British Army's primary battle tank?",
+                                    "In which year did the Royal Air Force (RAF) become an independent branch of the UK's armed forces?",
+                                    "What is the UK's nuclear deterrent system called, which consists of Trident II D5 ballistic missiles?",
+                                    "What is the highest military award that can be bestowed upon a member of the British Armed Forces for acts of conspicuous gallantry and valor in the face of the enemy?",
+                                    "What is the Royal Navy's nuclear-powered ballistic missile submarine class that carries the Trident II D5 missile system?" };
+        public String[] Answers = { "The British Army, the Royal Navy, and the Royal Air Force.",
+                                    "Field Marshal.",
+                                    "The HMS Queen Elizabeth, an aircraft carrier.",
+                                    "The SAS (Special Air Service)",
+                                    "The Falklands War took place in 1982.",
+                                    "The Challenger 2.",
+                                    "1918.",
+                                    "Trident, also known as the Trident nuclear program.",
+                                    "The Victoria Cross (VC).",
+                                    "Vanguard-class submarines." };
         public string p1ID;
         public string p2ID;
         public string P1User;
         public string P2User;
-
         public MinesweeperGame(int P1UID, int P2UID)
         {
 
             InitializeComponent();
-
+            InitializeGame(player1TableLayoutPanel, player1Buttons, player1IsMine);
+            InitializeGame(player2TableLayoutPanel, player2Buttons, player2IsMine);
+            DisplayPlayerScores();
             this.p1ID = P1UID.ToString();
             this.p2ID = P2UID.ToString();
             this.P1User = DatabaseCon.Datalist("Username", "Scoreboard", p1ID);
@@ -39,8 +68,12 @@ namespace Minesweeper_code
         private void InitializeGame(TableLayoutPanel tableLayoutPanel, Button[,] buttons, bool[,] Minehit)
         {
             InitializeGrid(tableLayoutPanel, buttons);
+
+            foreach (Button button in buttons)
+            {
+                button.Click += Button_Click;
+            }
             PutMines(Minehit);
-            PlayGame(P1User, P2User);
         }
         private void InitializeGrid(TableLayoutPanel tableLayoutPanel, Button[,] buttons)
         {
@@ -89,13 +122,119 @@ namespace Minesweeper_code
             CellData cellData = (CellData)button.Tag;
             int row = cellData.Row;
             int col = cellData.Column;
+            if (turnCounter % QuestionsInterval == 0)
+            {
+                AskQuestion();
+            }
+            if (turnCounter % 2 == 0)
+            {
+                if (player1IsMine[row, col])
+                {
+                    MessageBox.Show("Player 1 hit a bomb! Game over for Player 1.");
+                    // Implement game-over logic for Player 1
+                    return;
+                }
+                else
+                {
+                    player1Score++;
+                }
+            }
+            else
+            {
+                if (player2IsMine[row, col])
+                {
+                    MessageBox.Show("Player 2 hit a bomb! Game over for Player 2.");
+                    // Implement game-over logic for Player 2
+                    return;
+                }
+                else
+                {
+                    player2Score++;
+                }
+            }
+            turnCounter++;
+            SwitchTurns();
+            DisplayPlayerScores();
         }
 
+
+        private void AskQuestion()
+        {
+            // Display a random question from the array
+            string question = GetRandomQuestion();
+            DialogResult result = MessageBox.Show(question, "Question", MessageBoxButtons.YesNo);
+
+            // If the answer is correct, give the player an extra turn
+            if (result == DialogResult.Yes)
+            {
+                if (turnCounter % 2 == 0)
+                {
+                    player1Score++;
+                }
+                else
+                {
+                    player2Score++;
+                }
+
+                // Update turn counter to account for the extra turn
+                turnCounter++;
+            }
+        }
+        private void DisplayPlayerScores()
+        {
+            // Display player scores in the UI
+            player1ScoreLabel.Text = $"Player 1 Score: {player1Score}";
+            player2ScoreLabel.Text = $"Player 2 Score: {player2Score}";
+        }
+        private void SwitchTurns()
+        {
+            // Switch turns between players
+            if (turnCounter % 2 == 0)
+            {
+                player1TableLayoutPanel.Enabled = true;
+                player2TableLayoutPanel.Enabled = false;
+            }
+            else
+            {
+                player1TableLayoutPanel.Enabled = false;
+                player2TableLayoutPanel.Enabled = true;
+            }
+        }
+        private string GetRandomQuestion()
+        {
+            Random random = new Random();
+            int index = random.Next(questions.Count);
+            return questions[index];
+        }
+
+        private int CountAdjacentMines(int row, int col)
+        {
+            int count = 0;
+            bool[,] currentPlayerMines = (turnCounter % 2 == 0) ? player1IsMine : player2IsMine;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    int newRow = row + i;
+                    int newCol = col + j;
+
+                    if (newRow >= 0 && newRow < Rows && newCol >= 0 && newCol < Columns)
+                    {
+                        if (currentPlayerMines[newRow, newCol])
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
         public void button1_Click(object sender, EventArgs e)
         {
-            // NOT IN USE
+            ///
         }
-
 
         private void MinesweeperGame_Load(object sender, EventArgs e)
         {
@@ -104,51 +243,18 @@ namespace Minesweeper_code
             label3.Text = P1User + "has cleared 0 tiles";
             label4.Text = P2User + "has cleared 0 tiles";
         }
-        public void player1go(bool foundmin3)
-        {
-
-        }
-        public void PlayGame(string P1, string P2)
-        {
-            int i1 = 0;
-            int i2 = 0;
-            bool foundmine = false;
-            while (foundmine = false && i1 + i2 < 100)
-            {
-                int i = i1 + i2;
-                if (i % 2 == 0)
-                {
-                    pictureBox3.SendToBack();
-                    PlayerTurn(P1, i1);
-                    i1++;
-                    if (i1 % 3 == 0)
-                    {
-                        MilQuestion();
-                    }
-                }
-                else
-                {
-                    pictureBox1.SendToBack();
-                    PlayerTurn(P2, i2);
-                    i2++;
-                }
-            }
-        }
-        public void PlayerTurn(string player, int turnnumber)
-        {
-            
-        }
-        public bool MilQuestion()
-        {
-            String[] Questions = { "What is the official name of the UK's Army, Navy, and Air Force?", "What is the highest rank in the British Army?", "What is the largest and most powerful warship in the Royal Navy's fleet?", "What is the British Army's elite special forces unit known as?", "Which conflict is often referred to as the Falklands War?", " in which the UK fought against Argentina?", "What is the British Army's primary battle tank?", "In which year did the Royal Air Force (RAF) become an independent branch of the UK's armed forces?", "What is the UK's nuclear deterrent system called, which consists of Trident II D5 ballistic missiles?", "What is the highest military award that can be bestowed upon a member of the British Armed Forces for acts of conspicuous gallantry and valor in the face of the enemy?", "What is the Royal Navy's nuclear-powered ballistic missile submarine class that carries the Trident II D5 missile system?" };
-            String[] Answers = { "The British Army, the Royal Navy, and the Royal Air Force.", "Field Marshal.", "The HMS Queen Elizabeth, an aircraft carrier.", The SAS (Special Air Service).\r\n\r\nThe Falklands War took place in 1982.\r\n\r\nThe Challenger 2.\r\n\r\nThe Royal Air Force was founded on April 1, 1918.\r\n\r\nTrident, also known as the Trident nuclear program.\r\n\r\nThe Victoria Cross (VC).\r\n\r\nVanguard-class submarines." };
-        }
 
     }
+
+
+
+}
+
     class CellData
     {
         public int Row { get; }
-        public int Column { get; }" , "        public CellData(int row, int column)
+        public int Column { get; }
+        public CellData(int row, int column)
         {
             Row = row;
             Column = column;
--- Minesweeper code/Minesweeper code/Rules.cs
+++ Minesweeper code/Minesweeper code/Rules.cs
@@ -9,7 +9,7 @@ namespace Minesweeper_code
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) // create account form
         {
             CreateAccount newform = new CreateAccount();
             this.Hide();
@@ -19,8 +19,8 @@ namespace Minesweeper_code
         //osgbisebg
         private void button2_Click(object sender, EventArgs e)
         {
-            const string provider = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = M:/Projects/MineScoreboard1.accdb";
-
+            /*const string provider = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = M:/Projects/MineScoreboard1.accdb";
+            // database
             List<string> retrievedP1PasswordResults = new List<string>();
             string CheckP1HashPass = Hasher.Hash(textBox2.Text, 15);
             List<string> P1UID;
@@ -29,11 +29,21 @@ namespace Minesweeper_code
                 retrievedP1PasswordResults = DatabaseCon.Datalist("Password", "Scoreboard", "Username", textBox1.Text);
                 P1UID = DatabaseCon.Datalist("UID", "Scoreboard", "Username", textBox1.Text);
             }
+
+            // creates a list and checks the hashed password input with the hashed passwords within the database
+            
+            if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0 || textBox3.Text.Length == 0 || textBox4.Text.Length == 0)
+            {
+                MessageBox.Show("Not all boxes have been filled in.");
+                return;
+            }
+
             if (retrievedP1PasswordResults.Count.Equals(0))
             {
                 MessageBox.Show("Player 1's username or password is incorrect.");
+                return;
             }
-
+            
             List<string> retrievedP2PasswordResults = new List<string>();
             string CheckP2HashPass = Hasher.Hash(textBox4.Text, 15);
             List<string> P2UID;
@@ -45,21 +55,15 @@ namespace Minesweeper_code
             if (retrievedP2PasswordResults.Count.Equals(0))
             {
                 MessageBox.Show("Player 2's username or password is incorrect.");
-            }
-
-            if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0 || textBox3.Text.Length == 0 || textBox4.Text.Length == 0)
-            {
-                MessageBox.Show("Not all boxes have been filled in.");
+                return;
             }
 
             if (retrievedP2PasswordResults.Contains(CheckP2HashPass) && retrievedP1PasswordResults.Contains(CheckP1HashPass))
-            {
+            {*/
                 MessageBox.Show("Game Started.");
-                MinesweeperGame newform = new MinesweeperGame(Convert.ToInt32(P1UID[0]), Convert.ToInt32(P2UID[0]));
-                this.Hide();
-                newform.ShowDialog();
-                this.Close();
-            }
+            //MinesweeperGame newform = new MinesweeperGame(Convert.ToInt32(P1UID[0]), Convert.ToInt32(P2UID[0]));
+
+            //}
         }
     }
 }
